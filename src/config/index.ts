a7e3d9bad6e911e9149a2031cf56e427fb22d26a
Natalie Davis <natalienetworks@gmail.com>
2025-02-20 20:02:20 -0600

    update env and zod

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,11 +7,14 @@ dotenv.config();
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']),
   PORT: z.string().transform(Number),
-  DB_HOST: z.string(),
   DB_USER: z.string(),
   DB_PASSWORD: z.string(),
+  DB_PORT: z.string().transform(Number),
+  DB_HOST: z.string(),
   DB_NAME: z.string(),
-  APP_SECRET: z.string().min(32),
+  ENCRYPTION_KEY: z.string().min(32),
+  NEXT_PUBLIC_BSKY_BASE_API_URL: z.enum(['https://bsky.social']),
+  CLIENT_URL: z.string(),
 });
 
 export const config = envSchema.parse(process.env);
