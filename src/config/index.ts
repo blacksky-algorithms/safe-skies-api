98a59fa18da7defc34ff13e5a61c61c2325b50f1
nulfrost <khadane.miller@gmail.com>
2025-04-12 11:57:59 -0400

    chore: get env files working in docker environment

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,15 @@
-import { z } from 'zod';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { z } from "zod";
+import "dotenv/config";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'test']),
+  NODE_ENV: z.enum(["development", "production", "test"]),
   PGPORT: z.string().transform(Number),
   PGUSER: z.string(),
   PGPASSWORD: z.string(),
   PGHOST: z.string(),
   PGDATABASE: z.string(),
   ENCRYPTION_KEY: z.string().min(32),
-  BSKY_BASE_API_URL: z.enum(['https://api.bsky.app']),
+  BSKY_BASE_API_URL: z.enum(["https://api.bsky.app"]),
   CLIENT_URL: z.string(),
   PORT: z.string().transform(Number),
   RSKY_FEEDGEN: z.string(),
